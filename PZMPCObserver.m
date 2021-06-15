b9feae11e64b5b3c53c82faaa92834a9dfe828fd
Armin Ekic <armin.ekic@ndsu.edu>
2021-06-15 13:30:14 -0500

    system controllable with basic observer implementation

--- PZMPCObserver.m
+++ PZMPCObserver.m
@@ -6,30 +6,42 @@
 
 Utdi = 47.2;
 
-A = [2.91, -2.822625, 0.9126005; 1, 0, 0; 0, 1, 0];
+A = [2.91,-2.822625,0.9126005; 1,0,0; 0,1,0];
 B = [-3.969/Utdi; 0; 0];
-C = [0, 0, 1];
+C = [0,0,1];
 D = 0;
 
 Q = diag([1000,1000,1000]); %Given Q matrix 
-R = 15; %Given input weightine matrix
 
 t = [0:0.01:10]';
 
+R = 15; %Given input weighting matrix
+
+H = lqr(A',C',Q,R)';
+
+Kx = lqr(A,B,Q,R) %Maybe find Kx like Nate found it?
+DC = -C*inv(A-B*Kx)*B;
+Kr = 1/DC
+
 %%%%This here is what I am unsure on%%%%%%%%%%%%%
 
-H = ppl(A',C',[-1,-2,-3,-4])' %Arbitrarily placed poles
+%maybe these are the matrices to give us the gains, and the other matrices
+%come later??????? But these are basically whats shown in the book
 
-Aa = [A,zeros(3,3); H*C, A-H*C] %Augmented A matrix 
-Ba = [B;B] %Augmented B matrix
-Ca = diag([1,1,1,1,1,1]) %Augmented C matrix
-Da = zeros(6,6) %Augmented D Matrix
+% Aa = [A,zeros(3,3); H*C, A-H*C] %Augmented A matrix 
+% Ba = [B;B] %Augmented B matrix
+% Ca = diag([1,1,1,1,1,1]) %Augmented C matrix
+% Da = zeros(6,1) %Augmented D Matrix
+A6 = [A,-B*Kx; H*C,A-B*Kx-H*C]
+B6 = [B*Kr;B*Kr]
+C6 = diag([1,1,1,1,1,1])
+D6 = zeros(6,1)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-setPoint = 0*t+110; %Arbitrarily set to day values for entire duration
+setPoint = 0*t + 1; %Arbitrarily set to day values for entire duration
 
-X0 = zeros(9,1); %Initial conditions for the system, set to 0
+X0 = zeros(6,1); %Initial conditions for the system, set to 0
 
-y = step3(Aa,Ba,Ca,Da,t,X0,setPoint);
-plot(t,y);
+y = step3(A6,B6,C6,D6,t,X0,setPoint);
+plot(t,y,t,setPoint,'--');
--- ppl.m
+++ ppl.m
@@ -2,13 +2,13 @@ function [Kx] = ppl(A, B, P)
 
 %Step1: Find the similarity transform to get you to controller canonical
 %form
-    T1 = [B, A*B, A*A*B, A*A*A*B]; %Find the controllability matrix to make sure it controllable
+    T1 = [B, A*B, A*A*B]; %Find the controllability matrix to make sure it controllable
     rankT1 = rank(T1); %If this is equal to the dimension of the B matrix we are controllable
 
     Pe = poly(eig(A));
-    T2 = [Pe(1:4); 0,Pe(1:3); 0,0,Pe(1:2); 0,0,0,Pe(1)]; %Find the matrix related to the systems char. polynomial
+    T2 = [Pe(1:3); 0,Pe(1:2); 0,0,Pe(1)]; %Find the matrix related to the systems char. polynomial
 
-    T3 = [0,0,0,1; 0,0,1,0; 0,1,0,0; 1,0,0,0]; %Define the flip matrix
+    T3 = [0,0,1; 0,1,0; 1,0,0]; %Define the flip matrix
 
     T = T1*T2*T3; %Find the transform to get to controller canonical form
 
@@ -18,7 +18,7 @@ function [Kx] = ppl(A, B, P)
 %Step2: Find the full-state feedback gains in controller form
     Pd = poly(P); %Polynomial form of the desired poles
     dP = Pd - Pe; %Difference between desired and "current" poles
-    Kz = dP([5,4,3,2]); %Honestly, I'm not sure what this is doing
+    Kz = dP([4,3,2]); %Honestly, I'm not sure what this is doing
     
 %Step3: Convert Kz to the gain times the state variables
     Kx = Kz*inv(T); 
